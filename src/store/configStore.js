d2efb076d564389af7224fa61a496fc214058555
Hai Vu <vietnam5583@gmail.com>
2018-08-16 14:12:24 +0700

    Update redux-persists

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,20 +1,30 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from '~/src/store/reducers';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 let middleware = [];
 
 if (__DEV__) {
-    // const reduxImmutableStateInvariant = require('redux-immutable-state-invariant').default();
     middleware = [...middleware, logger];
 } else {
     middleware = [...middleware];
 }
+// const enhancer = [autoRehydrate(), applyMiddleware(...middleware)]
 
-export default function configureStore(initialState={}) {
-    return createStore(
-        rootReducer,
+export default function configureStore(initialState = {}) {
+    let store = createStore(
+        persistedReducer,
         initialState,
         applyMiddleware(...middleware)
-    );
+    )
+
+    return store
 }
