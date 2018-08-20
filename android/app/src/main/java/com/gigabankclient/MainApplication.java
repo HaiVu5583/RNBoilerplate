a65d2c92e7e81d236335c22f212aa0f292956ef7
Hai Vu <vietnam5583@gmail.com>
2018-08-20 09:24:40 +0700

    Remove unnecessary code

--- android/app/src/main/java/com/gigabankclient/MainApplication.java
+++ android/app/src/main/java/com/gigabankclient/MainApplication.java
@@ -1,61 +1,13 @@
 package com.gigabankclient;
-
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
-import com.horcrux.svg.SvgPackage;
-import com.ninty.system.setting.SystemSettingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
 
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
-import com.oblador.vectoricons.VectorIconsPackage;
-
-//public class MainApplication extends Application implements ReactApplication {
-//
-//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-//    @Override
-//    public boolean getUseDeveloperSupport() {
-//      return BuildConfig.DEBUG;
-//    }
-//
-//    @Override
-//    protected List<ReactPackage> getPackages() {
-//      return Arrays.<ReactPackage>asList(
-//          new MainReactPackage(),
-//            new RNCameraPackage(),
-//            new SvgPackage(),
-//            new SystemSettingPackage(),
-//            new VectorIconsPackage()
-//      );
-//    }
-//
-//    @Override
-//    protected String getJSMainModuleName() {
-//      return "index";
-//    }
-//  };
-//
-//  @Override
-//  public ReactNativeHost getReactNativeHost() {
-//    return mReactNativeHost;
-//  }
-//
-//  @Override
-//  public void onCreate() {
-//    super.onCreate();
-//    SoLoader.init(this, /* native exopackage */ false);
-//  }
-//}
 
 
 public class MainApplication extends NavigationApplication {
