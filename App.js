93e0d08f128637e23c302aa66046e8b91defad5b
Hai Vu <vietnam5583@gmail.com>
2018-08-25 09:43:18 +0700

    Restore language when open app

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ const store = configStore()
 import { BOTTOM_TABS } from '~/src/constants'
 import { persistStore } from 'redux-persist'
 import { DEFAULT_PUSH_ANIMATION, DEFAULT_POP_ANIMATION } from '~/src/themes/common'
+import { languageSelector } from '~/src/store/selectors/ui'
+import I18n from '~/src/I18n'
 
 const _getBottomTabIcon = (tabs, size, color) => {
     const promiseList = []
@@ -34,7 +36,7 @@ const _getBottomTabs = (bottomTabs) => {
 }
 
 
-const persist = (store) => {
+const _persist = (store) => {
     return new Promise(resolve => {
         persistStore(store, undefined, () => {
             resolve();
@@ -42,13 +44,55 @@ const persist = (store) => {
     })
 }
 
+const _setRoot = (bottomTabs) => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                id: 'mainStack',
+                children: [
+                    // {
+                    //     sideMenu: {
+                    //         id: 'sideMenu',
+                    //         left: {
+                    //             component: {
+                    //                 name: 'gigabankclient.SplashScreen',
+                    //             }
+                    //         },
+                    //         center: {
+                    //             bottomTabs: {
+                    //                 id: 'bottomTabs',
+                    //                 children: _getBottomTabs(bottomTabs)
+                    //             }
+                    //         },
+                    //     }
+                    // },
+                    {
+                        component: {
+                            name: 'gigabankclient.Authentication'
+                        }
+                    }
+                ]
+            },
+        }
+    })
+}
+
 YellowBox.ignoreWarnings([
     'Warning: Module SafeAreaManager requires',
 ]);
 
+let loadStoreDone = false
+let intervalID = -1
+
 export const run = () => {
-    console.log('Run Store', store)
-    persist(store)
+    _persist(store).then(() => {
+        loadStoreDone = true
+        // Apply language after restore store
+        const state = store.getState()
+        const language = languageSelector(state)
+        I18n.locale = language.toLowerCase()
+    })
+
     registerScreens(store)
     Navigation.events().registerAppLaunchedListener(() => {
         Navigation.setDefaultOptions({
@@ -74,40 +118,16 @@ export const run = () => {
         })
 
         _getBottomTabIcon(BOTTOM_TABS, 24, '#F16654').then(bottomTabs => {
-            Navigation.setRoot({
-                root: {
-                    stack: {
-                        id: 'mainStack',
-                        children: [
-                            // {
-                            //     sideMenu: {
-                            //         id: 'sideMenu',
-                            //         left: {
-                            //             component: {
-                            //                 name: 'gigabankclient.SplashScreen',
-                            //             }
-                            //         },
-                            //         center: {
-                            //             bottomTabs: {
-                            //                 id: 'bottomTabs',
-                            //                 children: _getBottomTabs(bottomTabs)
-                            //             }
-                            //         },
-                            //     }
-                            // },
-                            {
-                                component: {
-                                    name: 'gigabankclient.Authentication'
-                                }
-                            }
-                        ]
-                    },
-                }
-            })
-
+            if (loadStoreDone) {
+                _setRoot(bottomTabs)
+            } else {
+                invervalID = setInterval(() => {
+                    if (loadStoreDone) {
+                        _setRoot(bottomTabs)
+                        clearInterval(intervalID)
+                    }
+                }, 50)
+            }
         })
-
-
     })
-
 }
