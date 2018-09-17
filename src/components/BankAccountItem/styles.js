499b3d619aedbd0747b27c8257b59e4f4843c2e1
Hai Vu <vietnam5583@gmail.com>
2018-09-17 11:11:16 +0700

    Update BankAccountItem

--- src/components/BankAccountItem/styles.js
+++ src/components/BankAccountItem/styles.js
@@ -11,5 +11,21 @@ export default {
         padding: 5,
         borderRadius: 10,
         backgroundColor: COLORS.LIGHT_GRAY
+    },
+    iconContainer: {
+        position: 'absolute', 
+        right: 0,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        padding: 10,
+        zIndex: -1,
+        justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'row',
+        backgroundColor: COLORS.LIGHT_GRAY,
+        borderRadius: 10
+    },
+    icon: {
+        fontSize: 24, 
+        color: COLORS.DARK_BLUE,
     }
 }
